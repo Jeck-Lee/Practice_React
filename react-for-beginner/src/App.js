4a9ec1bf5e75a7575c655aabfeef281f5818be2f
Jeck-Lee <jecklee02@gmail.com>
2023-07-07 16:07:12 +0900

    Coin Tracker

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,39 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    // state가 const 변수이므로 직접 수정할 수 없음 (ex.todos.push("element");)
-    setTodos((currentArray) => [todo, ...currentArray]);
-    setTodo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [Coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({todos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={todo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
+      <h1>The Coins! ({Coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
       <ul>
-        {
-          // Array 안의 모든 element에 대해 실행되고, 실행된 값의 결과로 이뤄진 새로운 Array를 return
-          todos.map((item, index) => (
-            // Each child in a list should have a unique "key" prop.
-            <li key={index}>{item}</li>
-          ))
-        }
+        {Coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </li>
+        ))}
       </ul>
     </div>
   );
