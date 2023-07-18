e8ae9cdceff8c47438b456a92a7e07a5a747be2e
Jeck-Lee <jecklee02@gmail.com>
2023-07-18 23:21:44 +0900

    React Router & useParams (Link to)

--- react-for-beginner/src/routes/Detail.js
+++ react-for-beginner/src/routes/Detail.js
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState();
+
+  const getMovie = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+    );
+    const json = await response.json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.summary}
+            genres={movie.genres}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
